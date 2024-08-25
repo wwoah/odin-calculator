a88f8a6352d3e4e49df09e638f3726558a1d252e
wwoah <phillipkbishop@gmail.com>
2024-08-25 15:34:20 -0700

    Partial Refactor to make html elements dynamically produced

--- calculator.js
+++ calculator.js
@@ -1,21 +1,16 @@
 //Basic Calculator Functions
-//Addition
 function add(a,b){ return `${a+b}` };
-//Subtraction
 function sub(a,b){ return `${a-b}` };
-//Multiplication
 function mult(a,b){ return `${a*b}` };
-//Divsion
 function div(a,b){ return (b == 0 ? 'Nice Try' : `${(a/b).toFixed(3)}`);}
 
 //Operate function that takes in string input from a form
 function operate(expr) {
-    console.log(expr);
+    //console.log(expr);
     let exprArr = expr.split(/[\+\-\*\/]/);
     let opIndex = expr.search(/[\+\-\*\/]/);
-    console.log(exprArr);
-    console.log(`${opIndex} : ${expr[opIndex]}`);
-
+    //console.log(exprArr);
+    //console.log(`${opIndex} : ${expr[opIndex]}`);
 
     switch(expr[opIndex]) {
         case '+':
@@ -32,8 +27,24 @@ function operate(expr) {
 }
 
 //Load layout of Keyboard for Calculator
-
-
-
+const keys = ["AC","DE",".","/","7","8","9","*","4","5","6","-","1","2","3","+","0","="];
 
 //Dynamically place the keys
+//Reference container
+const form = document.querySelector("form");
+const display = document.createElement("div");
+display.classList.add("display");
+const inputDisplay = document.createElement("input");
+inputDisplay.type = "text";
+inputDisplay.name = "display";
+display.textContent = "Hello";
+display.style.color = "white";
+
+
+
+
+
+//Styling and qualities must be applied before appending
+display.appendChild(inputDisplay);
+form.appendChild(display);
+form.appendChild(key);
